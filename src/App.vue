7f4e08eef3e6ab2db032945d462d11d70a3c9af3
Musab Mohammed <mumusab268000@gmail.com>
2022-05-20 08:09:59 +0200

    create navbar and some files

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TopBar from './components/TopBar.vue';
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="">
+    <top-bar />
+  
+  
+  </div>
+</template>
