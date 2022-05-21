9d9915d7cfa5c69a69eaaf36392ef22500a08d80
Musab Mohammed <mumusab268000@gmail.com>
2022-05-21 16:25:41 +0200

    add body file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import TopBar from './components/TopBar.vue';
+import TheBody from './components/TheBody.vue';
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <div class="">
     <top-bar />
+
+    <the-body />
   
   
   </div>
