d134ab0fdedea0cc9cc6959818ea3c6026791744
Musab Mohammed <mumusab268000@gmail.com>
2022-05-22 20:49:37 +0200

    add photos and best selling

--- src/components/TheBody.vue
+++ src/components/TheBody.vue
@@ -6,10 +6,10 @@
 
 
         <!-- component -->
-        <div id="header" class="w-full">
+        <div id="header" class="w-full border-b-2 bg-white">
             <div class="flex" style="height:600px;">
                 <div class="hidden lg:block lg:w-1/2" style="clip-path:polygon(10% 0, 100% 0%, 100% 100%, 0 100%)">
-                    <div class="h-full object-cover" style="background-image: url(https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80)">
+                    <div class="h-full object-cover" style="background-image: url(https://images.unsplash.com/photo-1601924994987-69e26d50dc26?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170)">
                         <div class="h-full bg-black opacity-25"></div>
                     </div>
                 </div>
@@ -25,6 +25,106 @@
                 </div>
             </div>
         </div>
+
+
+
+        <h1 class="mt-12 text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl">الأكثر مبيعا</h1>
+
+
+        <!-- COMPONENT -->
+        <div class="grid grid-cols-1 gap-8 mx-8 mt-8 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
+
+            <div class="flex flex-col items-center justify-center w-full max-w-lg mx-auto bg-white border-2 shadow-md rounded-md overflow-hidden">
+                <img class="object-cover w-full h-72 xl:h-80" src="https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="T-Shirt">
+                <h4 class="mt-2 text-lg font-medium text-gray-700">قميص</h4>
+                <div class="w-full px-2">
+                    <span class="">تمساح<span></span></span>
+                    <div class="flex items-center mt-4 text-gray-700 ">
+                        <img class="h-6 ml-1" src="@/assets/svg/color.svg" alt="svg">
+    
+                        <div>
+                            <span class="mx-1 inline-flex px-4 py-2 bg-red-500 rounded-full"></span>
+                            <span class="mx-1 inline-flex px-4 py-2 bg-blue-500 rounded-full"></span>
+                        </div>
+                    </div>
+    
+                    <div class="flex items-center mt-1 mb-2 text-gray-700 ">
+                        <img class="h-6 ml-1" src="@/assets/svg/cash.svg" alt="svg">
+    
+                        <h1 class="px-2 text-lg font-extrabold">2000 <span class="mr-1 font-normal text-base">جنيه</span></h1>
+                    </div>
+                </div>
+            </div>
+<div class="flex flex-col items-center justify-center w-full max-w-lg mx-auto bg-white border-2 shadow-md rounded-md overflow-hidden">
+                <img class="object-cover w-full h-72 xl:h-80" src="https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="T-Shirt">
+                <h4 class="mt-2 text-lg font-medium text-gray-700">قميص</h4>
+                <div class="w-full px-2">
+                    <span class="">تمساح<span></span></span>
+                    <div class="flex items-center mt-4 text-gray-700 ">
+                        <img class="h-6 ml-1" src="@/assets/svg/color.svg" alt="svg">
+    
+                        <div>
+                            <span class="mx-1 inline-flex px-4 py-2 bg-red-500 rounded-full"></span>
+                            <span class="mx-1 inline-flex px-4 py-2 bg-blue-500 rounded-full"></span>
+                        </div>
+                    </div>
+    
+                    <div class="flex items-center mt-1 mb-2 text-gray-700 ">
+                        <img class="h-6 ml-1" src="@/assets/svg/cash.svg" alt="svg">
+    
+                        <h1 class="px-2 text-lg font-extrabold">2000 <span class="mr-1 font-normal text-base">جنيه</span></h1>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col items-center justify-center w-full max-w-lg mx-auto bg-white border-2 shadow-md rounded-md overflow-hidden">
+                <img class="object-cover w-full h-72 xl:h-80" src="https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="T-Shirt">
+                <h4 class="mt-2 text-lg font-medium text-gray-700">قميص</h4>
+                <div class="w-full px-2">
+                    <span class="">تمساح<span></span></span>
+                    <div class="flex items-center mt-4 text-gray-700 ">
+                        <img class="h-6 ml-1" src="@/assets/svg/color.svg" alt="svg">
+    
+                        <div>
+                            <span class="mx-1 inline-flex px-4 py-2 bg-red-500 rounded-full"></span>
+                            <span class="mx-1 inline-flex px-4 py-2 bg-blue-500 rounded-full"></span>
+                        </div>
+                    </div>
+    
+                    <div class="flex items-center mt-1 mb-2 text-gray-700 ">
+                        <img class="h-6 ml-1" src="@/assets/svg/cash.svg" alt="svg">
+    
+                        <h1 class="px-2 text-lg font-extrabold">2000 <span class="mr-1 font-normal text-base">جنيه</span></h1>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col items-center justify-center w-full max-w-lg mx-auto bg-white border-2 shadow-md rounded-md overflow-hidden">
+                <img class="object-cover w-full h-72 xl:h-80" src="https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="T-Shirt">
+                <h4 class="mt-2 text-lg font-medium text-gray-700">قميص</h4>
+                <div class="w-full px-2">
+                    <span class="">تمساح<span></span></span>
+                    <div class="flex items-center mt-4 text-gray-700 ">
+                        <img class="h-6 ml-1" src="@/assets/svg/color.svg" alt="svg">
+    
+                        <div>
+                            <span class="mx-1 inline-flex px-4 py-2 bg-red-500 rounded-full"></span>
+                            <span class="mx-1 inline-flex px-4 py-2 bg-blue-500 rounded-full"></span>
+                        </div>
+                    </div>
+    
+                    <div class="flex items-center mt-1 mb-2 text-gray-700 ">
+                        <img class="h-6 ml-1" src="@/assets/svg/cash.svg" alt="svg">
+    
+                        <h1 class="px-2 text-lg font-extrabold">2000 <span class="mr-1 font-normal text-base">جنيه</span></h1>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <h1 class="mt-12 text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl">الأكثر مبيعا</h1>
+
+        <!-- COMPONENT -->
 
 
     </div>
